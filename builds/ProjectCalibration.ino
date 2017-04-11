54ed3d8923a5f03a848557f133aa991fe0832600
Ashirdavid <davidashir@gmail.com>
2017-04-11 11:43:09 -0600

    Add files via upload

---
+++ builds/ProjectCalibration.ino
@@ -0,0 +1,21 @@
+/* This code was to both check and cailbrate the microphone to pick up specific sounds made*/
+
+
+#include <uspeech.h>
+signal voice(A0);
+void setup() {
+  int minVolume;
+  int fconstant;
+  int econstant;
+  int aconstant;
+  int vconstant;
+  int shconstant;
+voice.calibrate();
+voice.f_enabled = true;
+
+Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(voice.getPhoneme());
+}
